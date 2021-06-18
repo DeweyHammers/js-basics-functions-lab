36437ac434588569e898c314b65e50c9d3eae6bf
Dewey <hammersdrichard@gmail.com>
2021-06-18 07:13:57 -0700

    commit tree 451b23aa975b0627fc23025ef354483671fac885

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = (value) => {
+  return Math.abs(42 - value);
+}
+
+const distanceFromHqInFeet = (value) => {
+  const blocks = distanceFromHqInBlocks(value);
+  return blocks * 264;
+}
+
+const distanceTravelledInFeet = (start, destination) => {
+  return Math.abs((start - destination)*264);
+}
+
+const calculatesFarePrice = (start, destination) => {
+  let distance = Math.abs((start - destination)*264)
+
+  if (distance <= 400) {
+    return 0 
+  } else if (distance > 400 && distance < 2000) {
+    const cost = Math.abs(distance - 400) * 2
+    return parseFloat(`${cost.toString().slice(0, 1)}.${cost.toString().slice(1, 3)}`)
+  } else if (distance >= 2000 && distance < 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
